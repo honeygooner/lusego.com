936f89fc19b90786c9786cfc36511d501ab0c5f2
honeygooner <206956665+honeygooner@users.noreply.github.com>
2025-10-11 01:30:53 -0400

    chore: setup eslint (#10)
    * install dependencies
    
    * update package.json scripts
    
    * update .gitignore
    
    * update .vscode/settings.json
    
    * create eslint.config.mjs
    
    * replace dependency @eslint/compat with @eslint/eslintrc
    
    * update .vscode/extensions.json
    
    * update .vscode/settings.json
    
    * update .github/workflows/ci.yaml

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+export default defineConfig([
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  globalIgnores([".next/", "node_modules/", "out/", "next-env.d.ts"]),
+]);
