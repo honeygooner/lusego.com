226f5c40183778b59e27c1df723e04c8d54259d6
honeygooner <206956665+honeygooner@users.noreply.github.com>
2025-10-12 14:46:41 -0400

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants";
+
 /** @type {import("next").NextConfig} */
 const config = {
   typedRoutes: true,
 };
 
-export default config;
+/** @param {string} phase */
+export default async function getConfig(phase) {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const { default: withSerwistInit } = await import("@serwist/next");
+    const withSerwist = withSerwistInit({
+      swSrc: "app/sw.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(config);
+  }
+
+  return config;
+}
