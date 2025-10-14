2129db3cc29dc858fd047ba71902804323655fa8
honeygooner <206956665+honeygooner@users.noreply.github.com>
2025-10-14 18:13:07 -0400

    update svgr.d.ts
    re-added the declaration for "*.svg?url"

--- svgr.d.ts
+++ svgr.d.ts
@@ -3,3 +3,8 @@ declare module "*.svg" {
   const content: FC<SVGProps<SVGElement>>;
   export default content;
 }
+
+declare module "*.svg?url" {
+  const content: unknown;
+  export default content;
+}
