8714b6be7877026280c35c95a723d49a263f618a
honeygooner <206956665+honeygooner@users.noreply.github.com>
2025-10-14 18:04:27 -0400

    chore: setup svgr (#18)
    * install dependencies
    
    * update next.config.mjs
    
    * create svgr.d.ts
    
    * update tsconfig.json
    
    * update package.json
    
    removed usage of turbopack to improve compatibility with serwist
    (which does not yet support turbopack). also, as of writing, the
    installation guide for svgr is configured with webpack
    
    see:
    * https://github.com/serwist/serwist/issues/54
    * https://github.com/gregberge/svgr/blob/180eb6d/website/pages/docs/next.mdx#usage
    
    * update next.config.mjs
    
    opted to use the working implementation in the svgr documentation rather
    than the related issue as it lead to some build issue. also refactored
    the config file for clarity.

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,76 @@ import {
   PHASE_PRODUCTION_BUILD,
 } from "next/constants.js";
 
-/** @type {import("next").NextConfig} */
-const config = {
+export default setupSerwist({
   typedRoutes: true,
-};
+  webpack: setupSvgr({
+    icon: true,
+  }),
+});
 
-/** @param {string} phase */
-export default async function getConfig(phase) {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const { default: withSerwistInit } = await import("@serwist/next");
-    const withSerwist = withSerwistInit({
-      swSrc: "app/sw.ts",
-      swDest: "public/sw.js",
-    });
-    return withSerwist(config);
-  }
+/**
+ * @param {import("next").NextConfig} config
+ * @see {@link https://serwist.pages.dev/docs/next/getting-started | `@serwist/next` - serwist documentation}
+ */
+function setupSerwist(config) {
+  /** @param {string} phase */
+  return async (phase) => {
+    if (
+      phase === PHASE_DEVELOPMENT_SERVER ||
+      phase === PHASE_PRODUCTION_BUILD
+    ) {
+      const { default: withSerwistInit } = await import("@serwist/next");
+      const withSerwist = withSerwistInit({
+        swSrc: "app/sw.ts",
+        swDest: "public/sw.js",
+      });
+      return withSerwist(config);
+    }
 
-  return config;
+    return config;
+  };
+}
+
+/**
+ * note: this config is based on the svgr usage documentation for next.js found
+ * [here](https://github.com/gregberge/svgr/blob/785cba4/website/pages/docs/next.mdx).
+ * it makes some assumptions about the webpack rules applied by next.js and may
+ * not be as resilient to future changes.
+ * @param {import("@svgr/core").Config} options
+ * @see {@link https://react-svgr.com/docs/next | next.js - svgr documentation}
+ */
+function setupSvgr(options) {
+  /** @param {import("webpack").Configuration} config */
+  return (config) => {
+    // get the existing rule that handles .svg imports
+    const existingRule = config.module.rules.find((rule) =>
+      rule?.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      // reapply existing rule for .svg imports ending with "?url"
+      {
+        ...existingRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      // use svgr loader for all other .svg imports
+      {
+        test: /\.svg$/i,
+        issuer: existingRule.issuer,
+        resourceQuery: {
+          not: [...existingRule.resourceQuery.not, /url/],
+        },
+        use: {
+          loader: "@svgr/webpack",
+          options,
+        },
+      },
+    );
+
+    // modify existing rule to ignore .svg imports
+    existingRule.exclude = /\.svg$/i;
+
+    return config;
+  };
 }
